80d8df3dc3ff5e22a883591f816124e06f144571
Ross Glover <ross@ross.mayfirst.org>
2017-10-15 05:53:08 -0700

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        self.fail('Finish the test!')
+        # self.fail('Finish the test!')
 
         # She is invited to enter a to-do item immediately
 
@@ -40,7 +40,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(any(
-            row.text == '1: Buy peacock feathers' for row in rows))
+            row.text == '1: Buy peacock feathers' for row in rows),
+                        "New to-do item did not appear in table")
                         
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
@@ -55,4 +56,3 @@ class NewVisitorTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
